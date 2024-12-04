79811636bdb4b2291407bf69a6fc28b509371e11
Amirhossein Yaghoubi <ahyaghoubi07@gmail.com>
2024-12-05 00:10:27 +0330

    Enhance Profit Calculator with file-based result storage and input validation.
    - Updated the **Profit Calculator** project to include new features:
      - **File-based result storage:** Added functionality to save earnings before tax, earnings after tax, and earnings ratio to separate files.
      - **Improved input validation:** Added checks to ensure revenue, expenses, and tax rate are positive numbers, with appropriate error messages for invalid inputs.
    - Refactored the `printAndAssign` function to return errors for invalid input values.
    - Introduced a new `writeTofile` function to handle writing results to files with descriptive titles.
    - Enhanced overall robustness and usability of the program.

--- 2. Profit Calculator/Profit-calculator.go
+++ 2. Profit Calculator/Profit-calculator.go
@@ -1,24 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
 func main() {
-	revenue := printAndAssign("Enter the revenue amount: ")
-	expenses := printAndAssign("Enter the expenses amount: ")
-	taxRate := printAndAssign("Enter the tax rate: ")
+	revenue, err := printAndAssign("Enter the revenue amount: ")
+	if err != nil {
+		fmt.Println("ERROR:", err)
+		return
+	}
+
+	expenses, err := printAndAssign("Enter the expenses amount: ")
+	if err != nil {
+		fmt.Println("ERROR:", err)
+		return
+	}
+
+	taxRate, err := printAndAssign("Enter the tax rate: ")
+	if err != nil {
+		fmt.Println("ERROR:", err)
+		return
+	}
 
 	earningBeforeTax, earningAfterTax, ratio := Calculator(revenue, expenses, taxRate)
+
+	writeTofile("earningBeforeTax", earningBeforeTax)
+	writeTofile("earningAfterTax", earningAfterTax)
+	writeTofile("ratio", ratio)
 
 	fmt.Printf("Your earning before tax is: %.2f\n", earningBeforeTax)
 	fmt.Printf("Your earning after tax is: %.2f\n", earningAfterTax)
 	fmt.Printf("Your earning ratio is: %.2f\n", ratio)
 }
 
-func printAndAssign(text string) (result float64) {
+func writeTofile(title string, number float64) {
+	data := fmt.Sprint(number)
+	os.WriteFile(title, []byte(data), 0644)
+}
+
+func printAndAssign(text string) (result float64, err error) {
 	fmt.Print(text)
 	fmt.Scan(&result)
 
-	return result
+	if result <= 0 {
+		return 0, errors.New("invalid value")
+	}
+
+	return result, nil
 }
 
 func Calculator(revenue, expenses, taxRate float64) (ebt, eat, ratio float64) {
